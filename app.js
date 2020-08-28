9c724ec9a700f5196051e41296601f86cd74aeb9
Marika Könönen <marika.kononen@geniem.com>
2020-08-28 10:51:43 +0300

    Section 4: added yargs and add, read, remove and list commands

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes')
+const { describe } = require('yargs')
 
-const success = chalk.green
+const command = process.argv[2]
 
-const msg = getNotes()
-console.log(success(msg))
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command ({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note!')
+    }
+})
+
+// Create read command
+yargs.command ({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note!')
+    }
+})
+
+// Create list command
+yargs.command ({
+    command: 'list', 
+    describe: 'List your notes',
+    handler: function() {
+        console.log('Listing out all notes!')
+    }
+})
+
+console.log(yargs.argv)
 
